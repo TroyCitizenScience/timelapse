fe0bdafc3602c84356721bc93d79c8739ca03b0f
dylanrees <dylanrees@protonmail.ch>
2016-08-10 03:08:04 -0400

    updated capture script, added combine script

--- capture.sh
+++ capture.sh
@@ -1,6 +1,12 @@
-#this script does the actual capturing of images
-#to use it you'll need the "streamer" program installed and a webcam plugged in
-#you'll probably also want to set up a cronjob that runs this script
+#this shellscript does the actual capturing of images
+#in order to set this script up on Raspbian, run:
+
+#1.) sudo apt-get install streamer (command-line capture utility)
+#2.) make sure there is a directory called "timelapse" the same folder as this script
+#3.) make sure a webcam is plugged in
+#4.) set up a cronjob that runs the script (assuming you know how to do that)
+
+#you might want to just try running this script from the command line to make sure everything works
 
 declare -i count=0
 count=$(ls ./timelapse | wc -w)
---
+++ combine.sh
@@ -0,0 +1,10 @@
+#this shellscript combines your images into a gif
+#in order to get the software you need to run this script on Raspbian, run:
+
+#1.) sudo apt-get install imagemagick
+#2.) make sure this script is in the same folder as capture.sh and the "timelapse" folder
+
+#you can run this script manually or do it as a cronjob if you want
+
+#command below is filler until i customize it
+convert *.png[400x400] -splice 0x10 -background "#ffffff"  -append  -crop -0+10 output.png
